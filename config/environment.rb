0594b91af88eab3bacf6d1e526c73b71739c0670
rkartzman <remykartzman@gmail.com>
2015-02-02 16:59:44 -0500

    add dictionary to environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@
 #      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+require_relative '../words'
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Require gems we care about
