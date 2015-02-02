490e5059feae1932abf5683921538de3ce30184c
Justin Gaba & Jonathan Cho <pair+gabalicious.joncho26@devbootcamp.com>
2015-02-02 16:53:22 -0500

    Done with challanege

--- anagrams.rb
+++ anagrams.rb
@@ -1,12 +1,26 @@
 require 'sinatra'
 require 'json'
+require 'byebug'
+require_relative 'dict'
 
 get '/:word' do |word|
   content_type :json
+  # byebug
   # TODO: Return anagrams.
-  [].to_json
+  anagrams_for(word).to_json
 end
 
 get '/' do
   send_file 'index.html'
 end
+
+
+def anagrams_for(word)
+  matched = []
+  DICT.each { |dic_word|
+  matched << dic_word if word.downcase.split("").sort == dic_word.downcase.split("").sort
+  }
+  matched
+end
+
+
